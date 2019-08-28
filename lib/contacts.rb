c3c0bf2e191448fce6ca424ed8f401c53d11579b
Stacy Johns <stacykjohns@icloud.com>
2019-08-28 13:52:56 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,9 +15,9 @@ require 'pry'
    }
 
 def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
+  contacts.each do |person, details|
   if person == "Freddy Mercury"
-    contact_details_hash.each do |attribute, data|
+    details.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if {|ice_cream| ice_cream == "strawberry"}
       end
